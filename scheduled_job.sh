8301030ac401feaff66827251067f7fcdba17990
Michinobu Maeda <michinobumaeda@gmail.com>
2023-01-26 08:14:31 +0900

    bug fix

--- scheduled_job.sh
+++ scheduled_job.sh
@@ -2,6 +2,8 @@
 
 PHP_COMMAND=php
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
 $PHP_COMMAND artisan command:read_feed
 $PHP_COMMAND artisan command:dispatch_post
 $PHP_COMMAND artisan queue:work --once
